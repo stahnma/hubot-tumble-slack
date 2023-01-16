29899a054d743a5d65a5422d814df252ac839451
Michael Stahnke <stahnma@devops.af>
2023-01-16 16:47:14 -0600

    update files to load

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ const path = require('path')
 module.exports = (robot) => {
   const scriptsPath = path.resolve(__dirname, 'src')
   robot.loadFile(scriptsPath, 'tumble.coffee')
+  robot.loadFile(scriptsPath, 'quotes.coffee')
+  robot.loadFile(scriptsPath, 'delete_tumble.coffee')
 }
